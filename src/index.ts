4086de21d848bdfb89b571348bd8d78ae35c20f8
Eldad <eldadf456@gmail.com>
2023-10-25 16:52:21 +0300

    updated

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,22 @@
-import {User} from "./models/User";
+import {User} from './models/User';
 
-const user = new User({name: 'eldad', age: 25})
+const user  = new User({name: 'Eldad', age: 24});
 
-user.set({name: 'new NAME', age: 9999})
+class Person {
+    constructor(public firstName: string, public lastName: string) {
+    }
+        fullName(): string {
+            return `${this.firstName} ${this.lastName}`
+    }
+}
+
+const person = new Person('firstname','lastName' )
+console.log(person.fullName());
+
+const on = user.on;
+
+on('change', () => {
+
+})
 
 console.log(user.get('name'));
-console.log(user.get('age'));
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,18 +1,40 @@
-interface UsersProps {
-    name: string,
-    age: number
+
+import {Eventing} from "./Eventing";
+import {Sync} from "./Sync";
+import {Attributes} from './Attributes'
+
+export interface UsersProps {
+    id?: number,
+    name?: string,
+    age?: number
 }
+
+const roolUrl = 'http://localhost:3000/users';
+
 
 export class User {
+    public events: Eventing =  new Eventing()
+    public sync: Sync<UsersProps> = new Sync<UsersProps>(roolUrl);
+    public attribute: Attributes<UsersProps>;
 
-    constructor(private data: UsersProps ) {
+    constructor(attrs : UsersProps) {
+        this.attribute = new Attributes<UsersProps>(attrs);
     }
 
-    get(propName: string): string | number {
-        return this.data[propName];
+    on(eventName: string, callback: Callback) {
+        this.events.on(eventName,callback)
     }
 
-    set(update: UsersProps ) : void {
-    Object.assign(this.data, update);
+    get on() {
+        return this.events.on;
+    }
+
+    get trigger() {
+        return this.events.trigger;
+    }
+
+    get get() {
+        return this.attribute.get;
     }
 }
+
